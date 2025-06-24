9ebfb246fa975d6e71b376d89a55dd494369f001
Hanan Abdimalik <hanan.abdimalik@autotrader.co.uk>
2025-06-24 16:20:49 +0100

    First commit

---
+++ src/main/kotlin/com/games/joustservice/JoustServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.games.joustservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JoustServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<JoustServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/games/joustservice/JoustServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.games.joustservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JoustServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
